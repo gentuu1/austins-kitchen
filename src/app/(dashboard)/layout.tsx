2e39a2ef9d1f5fba0aae7e348ff2a1b66c4cfaf7
gentuu1 <azeezsodiq221@gmail.com>
2026-09-14 13:32:57 +0100

    changes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -33,7 +33,7 @@ export default function DashboardLayout({ children, }: { children: ReactNode }) 
     return (
         <div className="bg-zinc-50 w-screen h-screen lg:flex-row flex md:flex-col-reverse flex-col-reverse">
 
-            <div className={`border-t shadow-2xl lg:border-t-0 lg:shadow-none w-full lg:w-62 fixed bottom-0 left-0 md:fixed lg:relative lg:bg-zinc-100 bg-[#1F2933] z-50 `}>
+            <div className={`border-t shadow-2xl lg:border-t-0 lg:shadow-none w-full lg:w-62 fixed bottom-0 left-0 md:fixed lg:relative md:bg-zinc-100 bg-[#1F2933] z-50 `}>
 
                 <div className="relative lg:flex hidden items-center  w-full h-26 bg-[#1F2933]">
                     <div className="absolute left-1/2 transform -translate-x-1/2 h-20 w-32">
@@ -47,7 +47,7 @@ export default function DashboardLayout({ children, }: { children: ReactNode }) 
                     </div>
                 </div>
 
-                <div className=" w-full h-fit p-5 flex lg:flex-col lg:space-y-3 justify-between text-gray-700 ">
+                <div className=" w-full h-fit p-5 flex lg:flex-col lg:space-y-3 justify-between md:text-gray-700 text-white ">
 
                     <Link href='/dashboard'>
                         <div className={`${pathname == '/dashboard' && 'border border-[#ED8F0C]'} md:w-full flex items-center gap-3 p-3 rounded-lg cursor-pointer hover:bg-gray-100 hover:text-[#ED8F0C] transition md:flex-col lg:flex-row`}>
